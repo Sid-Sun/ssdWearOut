f07e2bb217362486da230befb80b34733ff921f6
Sidharth Soni <Sid-Sun@outlook.com>
2019-01-03 03:28:51 +0530

    Typo and UX fixes.
    Signed-off-by: Sid-Sun <sid-sun@outlook.com>

---
+++ README.md
@@ -0,0 +1,11 @@
+# ssdWearOut
+
+Do you want to wear out your SSD before the warranty expires?
+
+Don't worry, we've all been there, this script uses state-of-the-art* SSD wearing technology using AI* and ML* to wear out your SSD.
+
+*These are utterly false and no test of ANY sort were conducted.
+
+Please note that I am NOT responsibe for anything that may happen, use this script at your OWN risk and don't run it unless you understand what you are doing.
+
+Press the interrupt hotkeys to stop the script, it won't, otherwise; stop.
--- ssdWearOut.sh
+++ ssdWearOut.sh
@@ -1,13 +1,20 @@
 #Created by Sid Sun on 3 January 2019
 #Totally NOT based on a meme from, /r/ProgrammerHumor
 welcome() {
-	echo "Do you want to wear out your SSD before the warranty expires?"
-	echo "Don't worry, we've all been there, this script uses state-of-the-art* SSD wearing technology using AI* and ML* to wear out your SSD." #I've never been there, but; hey. whatever.
-	echo "*These are utterly false and no test of ANY sort were conducted."
-	echo "Please note that I am NOT responsibe for anything that may happen, use this script at your OWN risk and don't run it unless you understand waht you are doing."
-	echo "Press the interrupt hotkeys to stop the script, it won't, otherwist; stop."
-	read -n 1 -s -r -p "Press any key to continue."
-	echo " " #IT doesn't look good otherwise. UX is the top priority.
+	clear;
+	echo " ";
+	echo "Do you want to wear out your SSD before the warranty expires?";
+	echo " ";
+	echo "Don't worry, we've all been there, this script uses state-of-the-art* SSD wearing technology using AI* and ML* to wear out your SSD."; #I've never been there, but; hey. whatever.
+	echo " ";
+	echo "*These are utterly false and no test of ANY sort were conducted.";
+	echo " ";
+	echo "Please note that I am NOT responsibe for anything that may happen, use this script at your OWN risk and don't run it unless you understand what you are doing.";
+	echo " ";
+	echo "Press the interrupt hotkeys to stop the script, it won't, otherwise; stop.";
+	echo " ";
+	read -n 1 -s -r -p "Press any key to continue.";
+	echo " "; #IT doesn't look good otherwise. UX is the top priority.
 	wearOut;
 }
 wearOut() {
